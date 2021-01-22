02a86c3c4115c10aa1f6195db49fdfe3d783a899
Smeilb00 <36696341+Smeilb00@users.noreply.github.com>
2021-01-22 15:30:00 +0100

    Add files via upload

--- Sebas/Code/main.py
+++ Sebas/Code/main.py
@@ -1,6 +1,32 @@
-from codes import t5
-#t1.run()
-#t2.run()
-#t3.run()
-#t4.run()
-t5.run()
+from Codes import t1, t2, t3, t4, t5, t6, t7
+
+print("Que programa quieres ejecutar:")
+print("(Introducir número del 1 al 7)")
+num = int(input())
+
+if num == 1:
+    t1.run()
+elif num == 2:
+    t2.run()
+elif num == 3:
+    t3.run()
+elif num == 4:
+    t4.run()
+elif num == 5:
+    t5.run()
+elif num == 6:
+    t6.run()
+elif num == 7:
+    print("Introducir nombre de la carpeta donde se encuentra la imagen:")
+    inp = "\\" + str(input())
+    print("Introducir nombre de la carpeta del datapool:")
+    dat = "\\" + str(input())
+    print("Introducir nombre la carpeta de salida:")
+    out = "\\" + str(input())
+
+    data = {
+        "Input":inp,
+        "Output":out,
+        "Datapool": dat
+    }
+    t7.run(data)
